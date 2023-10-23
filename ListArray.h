ebc2858c84878256c7b3b711a0df3bd34db1d9be
Alberto Andres Gomez <albertoandresmotogp2@gmail.com>
2023-10-23 17:45:23 +0200

    Editado

--- ListArray.h
+++ ListArray.h
@@ -8,27 +8,18 @@ template <typename T>
 
 class ListArray : public List<T>{
 private:
-	T* arr;
-	T* arr_aux;
-	int max;
-	int n;
 	static const int MINSIZE = 2;
+	vector<T> arr[MINSIZE];;
+	vector<T> arr_aux[MINSIZE];
+	int max; //tamaño actual del array
+	int n; //numero de elementos que contiene la lista
+	
 	void resize(int new_size){
-		arr_aux = new int[new_size];
-		for(int i = 0; i < new_size; i++)
-			arr_aux[i] = arr[i];
-		
-		delete[] arr; //liberamos el espacio de memoria de arr, para el nuevo array.
-
-		arr = new int[new_size];
-		for(int j = 0; j < new_size; j++)
-			arr[j] = arr_aux[j];
-
-		max = new_size; //nuevo tamaño máximo de arr
+		arr.resize(new_size);
 	}
+	
 public:
 	ListArray(){
-		arr = new int[MINSIZE];
 		max = 2;
 		n = 0;
 	}	
@@ -46,21 +37,59 @@ public:
 	//friend ostream& operator<<(ostream &out, const ListArray<T> &list){
 
 	virtual void insert(int pos, T e) override{
-		if(pos < 0 || pos > arr.size() - 1)
+		if(pos < 0 || pos > arr.size())
 			throw out_of_range("La posición está fuera del intervalo permitido");				//tres casos posibles
 		if(pos == 0) //prepend
 			prepend(e);
 		else if(pos == n)//append
 			append(e);
-		else{
-			resize(arr.size() + 1);
-			for(int i = pos; i < max; i++){
-				arr[i] = arr[i + 1];
-			}
-			arr[pos] = e;
-		}
+		else
+			arr.insert(arr.begin() + pos, e);
 	}
 
-	//virtual void append(T e)
+	virtual void append(T e)override{
+		arr.push_back(e); //funcion que inserta elemento al final de la lista
+	}
+
+	virtual void prepend(T e)override{
+		arr.insert(arr.begin(), e);
+	}
+
+	virtual T remove(int pos)override{
+		if(pos < 0 || pos > arr.size())
+                        throw out_of_range("La posición está fuera del intervalo permitido");
+		vector<T> aux[1];
+		aux[1] = arr[pos];
+		arr.erase(arr.begin() + pos);
+
+		return aux[1];
+	}
+
+	virtual T get(int pos)override{
+		if(pos < 0 || pos > arr.size())
+                        throw out_of_range("La posición está fuera del intervalo permitido");
+
+		return pos;
+	}
+	
+	virtual int search(T e)override{
+		for(int i = 0; i < arr.size(); i++){
+			if(arr[i] = e)
+				return i;
+		}
+		return -1; //Si ha llegado a este return es que no ha encontrado el elemento e
+	}
+
+	virtual bool empty()override{
+		if(arr.size() == 0)
+			return true;
+		else
+			return false;
+	}
+
+	virtual int size()override{
+		return arr.size();
+	}
+
 
 };

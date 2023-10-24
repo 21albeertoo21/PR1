eebf757855913efb0a7dfa5534c7cd41817c5b9c
Alberto Andres Gomez <albertoandresmotogp2@gmail.com>
2023-10-24 23:42:35 +0200

    Listlinked.h añadido

---
+++ ListLinked.h
@@ -0,0 +1,17 @@
+#include <ostream>
+#include "List.h"
+#include "Node.h"
+
+using namespace std;
+
+template <typename T>
+
+class ListLinked : public List<T>{
+	private:
+		Node<T>* first;
+		int n; //nº de elementos de la lista	
+
+	
+
+};
+

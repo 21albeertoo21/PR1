a98f85224df509b7b8a680f1c8a52fac5d8c6296
Alberto Andres Gomez <albertoandresmotogp2@gmail.com>
2023-10-26 18:54:01 +0200

    Añadido

--- ListArray.h
+++ ListArray.h
@@ -1,5 +1,5 @@
 #include <ostream>
-#include <vector>
+#include <iostream>
 #include "List.h"
 
 using namespace std;
@@ -13,19 +13,28 @@ private:
         int n; //número de elementos de la lista
         static const int MINSIZE = 2;
         void resize(int new_size){
-                T* arr_aux = new T[new_size];
-                for(int i = 0; i < max; i++)
-                        arr_aux[i] = arr[i];
-
-                delete[] arr; //liberamos el espacio de memoria de arr, para el nuevo array.
-		
+        	T* arr_aux = new T[new_size];
+        	if(new_size < n){ //caso remove
+		        for(int i = 0; i < new_size; i++)
+			        arr_aux[i] = arr[i];
+			
+			n--;
+		}
+		else{ //caso insert
+			for(int i = 0; i < n; i++)
+				arr_aux[i] = arr[i];
+			for(int i = n; i < new_size; i++)
+				arr_aux[i] = 0;
+			
+			n++;
+		}
+		delete[] arr; //liberamos el espacio de memoria de arr, para el nuevo array.
 		arr = arr_aux;
-         
-                max = new_size; //nuevo tamaño de arr
+		max  = new_size; //nuevo tamaño de arr
         }
 public:
         ListArray(){
-                arr = new int[MINSIZE];
+                arr = new T[MINSIZE];
                 max = 2;
                 n = 0;
         }
@@ -34,7 +43,7 @@ public:
         }
 
         T operator[](int pos){
-                if(pos < 0 || pos > max - 1)
+                if(pos < 0 || pos >= n)
                         throw out_of_range("Numero introducido fuera de rango");
                 else
                         return arr[pos];
@@ -48,88 +57,78 @@ public:
 		return out;
 	}
 	
-	virtual void insert(int pos, T e) override{
-		if(pos < 0 || pos > max - 1)
-                        throw out_of_range("La posición está fuera del intervalo permitido");
-                
-		if(size() == max)
-			resize(max + 1);
-
+	void insert(int pos, T e) override{
+		if(pos < 0 || pos > n)
+                        throw out_of_range("La posición está fuera del intérvalo permitido");
+		resize(n + 1);
 		//tres casos posibles
-                if(pos == 0) //prepend
-                        prepend(e);
-                else if(pos == max - 1)//append
-                        append(e);
-                else{	
-			resize(max + 1);
-			for(int i = pos; i < max; i++){
-				arr[i] = arr[i + 1];
+		if(pos == n - 1){//append
+                        arr[pos] = e;
+		}
+                else{
+			for(int i = n - 1; i > pos; i--){
+				arr[i] = arr[i - 1];
 			}
 			arr[pos] = e;
-			n++;
+			for(int i = 0; i < n; i++)
+				cout << arr[i] << " ";
+			cout << endl;
                 }
         }
 
-        virtual void append(T e)override{
-		resize(max + 1);
-		arr[max - 1] = e;
-		n++;
+        void append(T e)override{
+		insert(n, e);
 	}
 
-	virtual void prepend(T e)override{
-		resize(max + 1);
-		for(int i = 0; i < max; i++){
-			arr[i] = arr[i + 1];
-		}
-		arr[0] = e;
-		n++;
+	void prepend(T e)override{
+		insert(0, e);
 	}
 
-	virtual T remove(int pos)override{
-		if(pos < 0 || pos > max - 1)
-                        throw out_of_range("La posición está fuera del intervalo permitido");
+	T remove(int pos)override{
+		if(pos < 0 || pos >= n)
+                        throw out_of_range("La posición está fuera del intérvalo permitido");
 		else{	
-			if(pos == max - 1){
+			if(pos == n - 1){
 				T aux = arr[pos];
-				resize(max - 1);
-				n--;
+				resize(n - 1);
 				return aux;
 			}
 			else{
 				T aux = arr[pos];
-				for(int i = pos; i < max - 1; i++)
+				for(int i = pos; i < n - 1; i++) //ponemos el valor que queremos eliminar en el último elemento del array y luego hacemos resize para eliminarlo
 					arr[i] = arr[i + 1];
-
-				resize(max - 1);
-				n--;
+				
+				arr[n - 1] = arr[pos];
+				resize(n - 1);
+				
 				return aux;
 			}	
 		}
 	}
 
-	virtual T get(int pos)override{
-		if(pos < 0 || pos > max - 1)
+	T get(int pos)override{
+		if(pos < 0 || pos >= n)
 			throw out_of_range("La posición está fuera del intérvalo permitido");
 		else
 			return arr[pos];
 	}
 
-	virtual int search(T e)override{
-		for(int i = 0; i < max; i++){
+	int search(T e)override{
+		for(int i = 0; i < n; i++){
 			if(arr[i] == e)
 				return i;
 		}
 		return -1;
 	}
 
-	virtual bool empty()override{
+	bool empty()override{
 		if(size() == 0)
 			return true;
 		else
 			return false;
 	}
 
-	virtual int size()override{
+	int size()override{
 		return n;
 	}
 };
